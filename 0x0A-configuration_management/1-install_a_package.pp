1cdee0bd9d77090e3385fbf44f4c8080de28de3f
othmane <othmane.alaouimekkaoui@gmail.com>
2024-01-20 16:45:38 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
-#!/usr/bin/pup
-## Install a given version of flask
-package {'flask':
- ensure   => '2.1.0',
- provider => 'pip3'
+# Using Puppet, install flask from pip3
+
+package { 'python3':
+    ensure   => installed,
 }
+
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    require  => Package['python3'],
+}
+
+package { 'Werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
+    require  => Package['flask'],
+}
+
