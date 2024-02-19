5d74d2bc98260d00af81e21bfbf8ea241ce0ba5f
othmane <othmane.alaouimekkaoui@gmail.com>
2024-02-19 10:58:05 -0800

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
+'''script that returns information about his/her TODO list progress'''
+
+import re
 import requests
 import sys
-import re
 
-
-REST_API = 'https://jsonplaceholder.typicode.com'
-
+REST_API = "https://jsonplaceholder.typicode.com"
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             id = int(sys.argv[1])
-            re = requests.get('{}/users/{}'.format(REST_API, id)).json()
-            task_re = requests.get('{}/todos'.format(REST_API)).json()
-            emp_nm = re.get('name')
-            task = list(filter(lambda x: x.get('userId') == id, task_re))
-            comp_task = list(filter(lambda x: x.get('completed'), task))
-            print('Employee {} is done with tasks({}/{}):'.format(
-                emp_nm, len(comp_task), len(task)))
-            if len(comp_task) > 0:
-                for tk in comp_task:
-                    print('\t {}'.format(tk.get('title')))
+            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            task_req = requests.get('{}/todos'.format(REST_API)).json()
+            emp_name = req.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
+            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    emp_name, len(completed_tasks), len(tasks))
+            )
+            if len(completed_tasks) > 0:
+                for task in completed_tasks:
+                    print('\t {}'.format(task.get('title')))
