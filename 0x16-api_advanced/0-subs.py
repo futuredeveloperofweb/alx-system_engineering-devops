63823fc4bc70c81062c86a8de1786bee7290d349
othmane <othmane.alaouimekkaoui@gmail.com>
2024-03-05 14:40:52 -0800

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''a function that queries the Reddit API and returns the number of
+subscribers for a given subreddit'''
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''returns the number of subscribers for a given subreddit'''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
+    try:
+        return url.get('data').get('subscribers')
+    except Exception:
+        return 0
+    return 0
+
+
+if __name__ == '__main__':
+    number_of_subscribers(argv[1])
