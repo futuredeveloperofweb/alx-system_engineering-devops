e890c5fb8f3c3bda72f7e6ec4bea95183a6c5e0c
othmane <othmane.alaouimekkaoui@gmail.com>
2024-03-07 16:40:37 -0800

    t0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
-'''a function that queries the Reddit API and returns the number of
-subscribers for a given subreddit'''
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            data = response.json().get('data')
-            if data:
-                subscribers = data.get('subscribers', 0)
-                return int(subscribers)
-        else:
-            return 0
-    except Exception as e:
-        return 0
-
-
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        print(number_of_subscribers(sys.argv[1]))
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        print("Usage: script.py <subreddit>")
+        return 0
