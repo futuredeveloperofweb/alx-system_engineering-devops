3654b34b0aa716c4cbb1c3ee40dbb12dbb7d880a
othmane <othmane.alaouimekkaoui@gmail.com>
2024-03-07 16:33:57 -0800

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-'''function that queries the Reddit API and returns the number of
+'''a function that queries the Reddit API and returns the number of
 subscribers for a given subreddit'''
-
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers for a given subreddit."""
+    """returns the number of subscribers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json().get('data')
+            if data:
+                subscribers = data.get('subscribers', 0)
+                return int(subscribers) 
+        else:
+            return 0
+    except Exception as e:
         return 0
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        print(number_of_subscribers(sys.argv[1]))
+    else:
+        print("Usage: script.py <subreddit>")
