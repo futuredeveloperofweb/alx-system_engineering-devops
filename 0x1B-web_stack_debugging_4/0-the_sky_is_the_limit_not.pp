d1d47005e55a55688ebc9824f520ff14806d54f0
othmane <othmane.alaouimekkaoui@gmail.com>
2024-03-11 21:07:29 -0700

    web_stack_debugging_4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# fix Nginx to get all the requests
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
